748c33f8b9ce5f9ef995e757ad7ced96be897153
amelini <antoniomelini@hotmail.com>
2023-06-22 12:50:54 -0300

    fixing some details

--- api/src/app.js
+++ api/src/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+
 app.use(morgan('dev'));
 app.use(cookieParser());
-
+app.use(express.json())
 app.use(cors())
 
 app.use("/api",routes);
